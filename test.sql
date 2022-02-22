d3867a89597c99a9cb886ecdb78a7e0ffef7dbeb
dxtiiin <100234726+dxtiiin@users.noreply.github.com>
2022-02-22 20:30:00 +0100

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,9 @@
+
+
+INSERT INTO `shops` (`store`, `item`, `price`) VALUES
+	('name vom store', 'bproof', 5000)
+;
+
+INSERT INTO `items` (`name`, `label`, `limit`) VALUES
+	('Weste', 'bproof', 15)
+;
